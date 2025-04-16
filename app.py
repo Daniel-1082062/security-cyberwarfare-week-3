21a5d70ec9d78d29082991554e477aed61f6f974
Daniel 1082062 <1082062@hr.nl>
2025-04-16 15:31:26 +0200

    Route gemaakt voor de add_student pagina met daarin requests om de pagina op te roepen en om een nieuwe student aan de database toe te voegen. Database wordt nu aangemaakt zodra app.py opgestart wordt.

--- app.py
+++ app.py
@@ -11,4 +11,30 @@ db = SQLAlchemy(app)
 def index():
     return 'Hello World!'
 
-app.run(debug=True)
+# importeer het Student model zodat ik een nieuwe entry in de tabel kan maken.
+from models import Student
+
+# Route om een nieuwe student toe te voegen
+@app.route('/add_student', methods=['GET', 'POST'])
+def add_student():
+    # Als de method POST is, maak een nieuwe rij in de tabel met als data de gegevens die in het formulier zijn ingevuld
+    if request.method == 'POST':
+        # definieer de waardes die in de kolommen moeten komen te staan
+        student_number = request.form['student_number']
+        name = request.form['name']
+        class_name = request.form['class_name']
+
+        # Maak een nieuwe rij in de tabel met de waardes die hierboven gedefinieerd zijn.
+        new_student = Student(student_number=student_number, name=name, class_name=class_name)
+        # Voeg de rij toe aan de database
+        db.session.add(new_student)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+# Als de method GET is, laat de pagina zien waarop je een nieuwe student kunt toevoegen
+return render_template('add_student.html')
+
+if __name__ == '__main__':if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
