1a5af38e98877bdeca063a12c3ceedfc2b84553a
Daniel 1082062 <1082062@hr.nl>
2025-05-11 21:42:23 +0200

    Model gemaakt voor de docenten en 1 admin docent aan de database toegevoegd

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Maak de kolommen aan voor de Student tabel in de database
 class Student(db.Model):
@@ -22,6 +23,7 @@ class StatementChoice(db.Model):
     choice_text = db.Column(db.String(360), nullable=False)
     choice_result = db.Column(db.String(1), nullable=False)
 
+# Maak de kolommen voor gemaakte keuzes van studenten
 class StudentChoice(db.Model):
     student_choice_id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable=False)
@@ -30,3 +32,18 @@ class StudentChoice(db.Model):
 
     student = db.relationship('Student', backref='student_choices')
     statement = db.relationship('Statement', backref='student_choices')
+
+# Maak de kolommen voor de docenten tabel in de database
+class Teacher(db.Model):
+    teacher_id = db.Column(db.Integer, primary_key=True)
+    teacher_name = db.Column(db.String(120), nullable=False)
+    teacher_username = db.Column(db.String(120), nullable=False)
+    teacher_password_hash = db.Column(db.String(120), nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def set_password(self, password):
+        self.teacher_password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.teacher_password_hash, password)
+
